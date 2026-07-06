0e96f860a7b5b19d4f5e6007a1fcdc5e1d3ba6bd
moonbag-pro <moonbag-pro@users.noreply.github.com>
2026-07-06 20:44:01 +1200

    Point WeCom userscript updates at fresh branch ref

--- userscripts/cin7-wecom-payment-message.user.js
+++ userscripts/cin7-wecom-payment-message.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cin7 WeCom Payment Message Sender
 // @namespace    livingculture
-// @version      4.5
+// @version      4.6
 // @description  Sends a WeCom payment message from Cin7 invoice/payment screen only.
 // @match        *://cin7.com/*
 // @match        *://*.cin7.com/*
@@ -9,8 +9,8 @@
 // @match        *://*.cin7core.com/*
 // @match        *://*.dearsystems.com/*
 // @match        https://inventory.dearsystems.com/*
-// @downloadURL  https://raw.githubusercontent.com/Livingculture/freight-tool/main/userscripts/cin7-wecom-payment-message.user.js
-// @updateURL    https://raw.githubusercontent.com/Livingculture/freight-tool/main/userscripts/cin7-wecom-payment-message.user.js
+// @downloadURL  https://raw.githubusercontent.com/Livingculture/freight-tool/refs/heads/main/userscripts/cin7-wecom-payment-message.user.js
+// @updateURL    https://raw.githubusercontent.com/Livingculture/freight-tool/refs/heads/main/userscripts/cin7-wecom-payment-message.user.js
 // @supportURL   https://github.com/Livingculture/freight-tool
 // @run-at       document-idle
 // @grant        GM_xmlhttpRequest
