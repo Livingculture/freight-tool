306a3569c908a4e17b9e8778e743f37e2d573ca2
LCPenrose <LCPenrose@users.noreply.github.com>
2026-08-28 15:58:06 +1200

    Replace Ibis shortcut with Teak Sofa

--- userscripts/omni-website-shortcuts.user.js
+++ userscripts/omni-website-shortcuts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Omni Living Culture Website Shortcuts
 // @namespace    livingculture-omni
-// @version      0.1.18
+// @version      0.1.19
 // @description  Adds Living Culture website shortcuts to the grey space between Cin7 Omni quote sections.
 // @match        https://go.cin7.com/Cloud/TransactionEntry/TransactionEntry.aspx*
 // @match        https://livingculture.co.nz/*
@@ -46,12 +46,12 @@
       url: 'https://livingculture.co.nz/collections/blinds'
     },
     {
-      label: 'Ibis',
-      url: 'https://livingculture.co.nz/search?q=ibis&options%5Bprefix%5D=last'
-    },
-    {
       label: 'Gas Fire Pits',
       url: 'https://livingculture.co.nz/collections/gas-fire-pits'
+    },
+    {
+      label: 'Teak Sofa',
+      url: 'https://livingculture.co.nz/collections/teak-lounge'
     },
     {
       label: 'Aluminium Sofa',
